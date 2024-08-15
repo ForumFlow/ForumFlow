8739b8232ce2f5258af79d51d3a08bb70a9ad73c
Anthony Duong <anthonyduong789@gmail.com>
2024-08-14 17:25:18 -0700

    added back showAllPresentations method to sqlUtilsCmds

--- packages/ForumFlowServer/SqlUtils/sqlUtilCmds.cs
+++ packages/ForumFlowServer/SqlUtils/sqlUtilCmds.cs
@@ -11,7 +11,7 @@ namespace ForumFlowServer.CreateTables
 
         private readonly string createTestDataCmd = "SqlUtils/DummyData.txt";
         //private readonly string createTestDataCmd = "TestingPresentation.txt";
- 
+
         private static SqliteConnection connection = new SqliteConnection("Data Source=forumflow.db");
 
         /// <summary>
@@ -47,21 +47,25 @@ namespace ForumFlowServer.CreateTables
             // }
         }
 
-        public void CreatePresentation(string title, string description, int authorId){
-            
-            
+        public void CreatePresentation(string title, string description, int authorId)
+        {
+
+
             connection.Open();
-            try {
-                using (var command = connection.CreateCommand()){
+            try
+            {
+                using (var command = connection.CreateCommand())
+                {
                     command.CommandText = "INSERT INTO Presentation (title, description, authorId) VALUES (@title, @description, @authorId)";
                     command.Parameters.AddWithValue("@title", title);
-                    command.Parameters.AddWithValue("@description", description);              
+                    command.Parameters.AddWithValue("@description", description);
                     command.Parameters.AddWithValue("@authorId", authorId);
                     command.ExecuteNonQuery();
                 }
             }
 
-            finally {
+            finally
+            {
                 connection.Close();
             }
         }
@@ -76,24 +80,24 @@ namespace ForumFlowServer.CreateTables
 
             using (var command = connection.CreateCommand())
             {
-            command.CommandText = "SELECT username, passwordSalt, passwordHash, firstName, lastName, ID FROM Users";
-            using (var reader = command.ExecuteReader())
-            {
-                if (reader.HasRows)
+                command.CommandText = "SELECT username, passwordSalt, passwordHash, firstName, lastName, ID FROM Users";
+                using (var reader = command.ExecuteReader())
                 {
-                while (reader.Read())
-                {
-                    var username = reader.GetString(0);
-                    var passwordSalt = reader.GetString(1);
-                    Console.WriteLine($"userName: {username}\n passwordSalt: {passwordSalt}\n passwordHash: {reader.GetString(2)}\n firstName: {reader.GetString(3)}\n lastName: {reader.GetString(4)}\n ID: {reader.GetInt32(5)}");
-                    Console.WriteLine("-------------------------------------------------");
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var username = reader.GetString(0);
+                            var passwordSalt = reader.GetString(1);
+                            Console.WriteLine($"userName: {username}\n passwordSalt: {passwordSalt}\n passwordHash: {reader.GetString(2)}\n firstName: {reader.GetString(3)}\n lastName: {reader.GetString(4)}\n ID: {reader.GetInt32(5)}");
+                            Console.WriteLine("-------------------------------------------------");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No users found.");
+                    }
                 }
-                }
-                else
-                {
-                Console.WriteLine("No users found.");
-                }
-            }
             }
         }
         public void showAllFaqs()
@@ -126,31 +130,62 @@ namespace ForumFlowServer.CreateTables
 
             connection.Close();
         }
+        public void showAllPresentations()
+        {
 
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT authorId, presentationId, title, description FROM Presentation";
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var authorID = reader.GetInt32(0);
+                            var presentationID = reader.GetInt32(1);
+                            var title = reader.GetString(2);
+                            var description = reader.GetString(3);
+
+
+                            Console.WriteLine($"PresentationID: {presentationID}\n Title: {title}\n Description: {description}\n AuthorID: {authorID}");
+                            Console.WriteLine("-------------------------------------------------");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No presentations found.");
+                    }
+                }
+            }
+
+        }
+
+
+
+
+
+
+
+        // public void ExecuteNonQuery(string query)
+        // {
+        //     using (var connection = new SqliteConnection(_connectionString))
+        //     {
+        //         connection.Open();
+
+
+        //         using (var command = connection.CreateCommand())
+        //         {
+        //             command.CommandText = query;
+        //             command.ExecuteNonQuery();
+        //         }
+        //     }
+        // }
     }
 
-
-
-
-
-
-
-    // public void ExecuteNonQuery(string query)
-    // {
-    //     using (var connection = new SqliteConnection(_connectionString))
-    //     {
-    //         connection.Open();
-
-
-    //         using (var command = connection.CreateCommand())
-    //         {
-    //             command.CommandText = query;
-    //             command.ExecuteNonQuery();
-    //         }
-    //     }
-    // }
 }
-
 
 
 
